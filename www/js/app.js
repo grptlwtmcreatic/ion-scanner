97f388e0d61367ec385d765c4ed619b93a4e57d2
josselin <clement.benoit7@gmail.com>
2017-03-28 14:13:23 +0200

    projet correct creatic
    refaire bouton retour
    verife internet, verif scanner,

--- www/js/app.js
+++ www/js/app.js
@@ -23,116 +23,61 @@ angular.module('starter', ['ionic','qrScanner'])
     }
   });
 })
-.controller('stopVideo', function($scope){
+.controller('homeController', function($scope){
+  console.log('coucou')
+  if (typeof window.localMediaStream !== 'undefined') {
+    console.log('je suis dans home')
+    window.localMediaStream.getVideoTracks()[0].stop()
+  }
+  
 
 })
-
 //Controleur pour saisir ou modifier une fiche produit.
-
 .controller('saisirController', ['$scope', '$http','$state', function($scope, $http, $state){
-
 //On lance le scanner et on récupère dans data l'adresse encodée.
+  
+  $("#showScan").click(function(){
+    $('#qr-scanner').css({"display":"block"})
+    $('#showScan').css({"display":"none"})
 
-   $scope.onSuccess = function(data) {
+  })
+
+  $scope.onSuccess = function(data) {
 
       $read = data;
       console.log(data);
 
-//On récupère l'id unique du QR pour le mettre dans le formulaire
-
+      //On récupère l'id unique du QR pour le mettre dans le formulaire
       id_qr_code = $read.replace("http://quai-lab.com/?pr_inventory=product&pr_qrcode=","");
       $('input[type=hidden]').val(id_qr_code);
       $('#id_qr_code1').html("Le QR Code est récupéré.");
         
-      if (id_qr_code != null){
-                    
-          console.log(id_qr_code);
+        if (id_qr_code != null){
+            console.log(id_qr_code);
+            $('#qr-scanner').css({"display":"none"})
+            $('#showScan').css({"display":"block"})
 
-//On récupère le json associé à l'adresse encodée et si il n'est pas vide, on remplit les champs du formulaire.
-
-          var json = $.getJSON($read);
-                    
-          if (json != null){
-            $read = $read.replace("product", "quickview");
-            $.getJSON($read, function(data) {
-              $('#libelle').val(data.title);
-              $('#description').val(data.description);
-              $img = data.img;
-              jQuery('#img').attr('src', $img);
-            });
-          }
-      }
-
-        
-        /*$scope.save = function(){
-          
-          console.log("je suis dans la fonction save")
-
-            
-         // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
- 
-        
- 
-          libelle = $('#libelle').val();
-          image = $('#image').val();
-          description = $('#description').val();
-          qrcode = $('#qrcode').val();
- 
-          
-         /*$http.post(
-            'http://quai-lab.com/?pr_inventory=add',
-            {'libelle ': libelle, 'description' : description, 'image' : image, 'qrcode' : qrcode},
-            'contenttype'
-          ).then(function successCallback(response) {
-              // this callback will be called asynchronously
-              // when the response is available
-              console.log('OK' + response)
-            }, function errorCallback(response) {
-              // called asynchronously if an error occurs
-              // or server returns response with an error status.
-              console.log('Erreur' + response)
-            });*/
-
-            //console.log(image)
-            /*$.ajax({
-                url: "http://quai-lab.com/?pr_inventory=add",
-                type: "POST",
-                data: {libelle : libelle, description : description, qrcode : qrcode},
-                
-                success: function(response, statut) {
-                    console.log(response)
-                    if(response == true) {
-                        console.log("j'ai bien envoyé");
-                        
-                        
-                        
-                    } else {
-                        console.log('Erreur : '+ response);
-                    }
-                },
-                error: function(response, statut) {
-                  console.log('ahhh' +response)
-                  
-                },
-
-                complete : function(resultat, statut){
-                  $state.go('home');
+            //On récupère le json associé à l'adresse encodée et si il n'est pas vide, on remplit les champs du formulaire.
+              $read = $read.replace("product", "quickview");
+              $.getJSON($read, function(data) {
+                if(data != null)
+                {
+                  $('#libelle').val(data.title);
+                  $('#description').val(data.description);
+                  $img = data.img;
+                  jQuery('#img').attr('src', $img);
                 }
-
-            });
-                
-          
-          
-        }*/
-
-
-    };
-    $scope.onError = function(error) {
-        console.log(error);
-    };
-    $scope.onVideoError = function(error) {
-        console.log(error);
-    }; 
+               
+              });
+            
+        }
+  };
+  $scope.onError = function(error) {
+    console.log(error);
+  };
+  $scope.onVideoError = function(error) {
+    console.log(error);
+  }; 
 
     $(function(){
           $('#saisirF').on('submit', function(e){
@@ -143,32 +88,26 @@ angular.module('starter', ['ionic','qrScanner'])
             var data = (formdata !== null) ? formdata : $form.serialize();
 
             $.ajax({
-              url: $form.attr('action'),
-              type: $form.attr('method'),
-              contentType: false,
-              processData: false,
-              dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-              data: data,
-              success: function (response) {
-                console.log("success")
-              },
-
-              error: function(response) {
-                console.log("error")
-                console.log(response)
-              },
-
-              complete : function(resultat, statut){
-                  $state.go('home');
+                url: $form.attr('action'),
+                type: $form.attr('method'),
+                contentType: false,
+                processData: false,
+                data: data,
+                success: function (response) {
+                  console.log("success")
+                  console.log(response)
+                },
+                error: function(response) {
+                  console.log("error")
+                  console.log(response)
+                },
+                complete : function(resultat){
+                    alert('fiche enregistrer')
+                    $state.go('home');
                 }
             });
-            
-            
-            
-            
           });
-        });
-
+    });
 }])
 
 //Scan pour s'authentifier.
@@ -180,21 +119,25 @@ angular.module('starter', ['ionic','qrScanner'])
         $http({
           method: 'GET',
           url: 'http://www.quai-lab.com/?pr_inventory=check_admin&pr_qrcode=' + data
-        }).then(function successCallback(response) {
-          console.log(response)
-          if (response.data == "1"){
-            console.log("OKKKKKKK")
-            $state.go('Saisir');
+        })
+        .then(
+          function successCallback(response) {
+            console.log(response)
+            if (response.data == "1"){
+              console.log("OKKKKKKK")
+              
+              $state.go('Saisir');
+              
+            }
+            else{
+              console.log("PAS OKKKKKKK")
+            }
+          }, 
+
+          function errorCallback(response) {
+              console.log("CaCA") 
           }
-          else{
-            console.log("PAS OKKKKKKK")
-          }
-          }, function errorCallback(response) {
-            console.log("CaCA")
-            
-          });
-                 
-        
+        );
     };
     $scope.onError = function(error) {
         console.log(error);
@@ -202,12 +145,7 @@ angular.module('starter', ['ionic','qrScanner'])
     $scope.onVideoError = function(error) {
         console.log(error);
     };
-
-    $scope.stopVideo = function(){
-      console.log("Video eteinte");
-    };
 }])
-
 
 .controller('scanController', ['$scope', function($scope) {
 
@@ -215,15 +153,13 @@ angular.module('starter', ['ionic','qrScanner'])
         
         console.log(data);
         $read = data;
-        $("#read").attr('href', $read);
-              $read = $read.replace("product", "quickview");
-                  $.getJSON($read, function(data) {
-                      $('#title').html(data.title);
-                      $img = data.img;
-                      $('#quickview').attr('src', $img);
-                  });
-
-        
+        $("#read").attr('onclick',"window.open('" + $read + "','_self')");
+        $read = $read.replace("product", "quickview");
+        $.getJSON($read, function(data) {
+              $('#title').html(data.title);
+              $img = data.img;
+              $('#quickview').attr('src', $img);
+        });
     };
     $scope.onError = function(error) {
         console.log(error);
@@ -231,7 +167,6 @@ angular.module('starter', ['ionic','qrScanner'])
     $scope.onVideoError = function(error) {
         console.log(error);
     };
-
 }])
 
 //http://quai-lab.com/?pr_inventory=product&pr_qrcode=584e5ff437551
